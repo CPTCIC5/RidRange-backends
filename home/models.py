35d281bb2622873227238ef83ffac508a399334b
p1yux <soulpiyush09@gmail.com>
2024-11-02 22:15:59 +0530

    khassi

--- home/models.py
+++ home/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from users.models import Courses,Semester
+from users.models import Courses,Semester,Subject,Profile
 # Create your models here.
 
 MSE_NUMBERING= (
@@ -12,12 +12,24 @@ class mseResult(models.Model):
     mse_number= models.IntegerField(choices=MSE_NUMBERING)
     course= models.ForeignKey(Courses, on_delete=models.CASCADE)
     semester= models.ForeignKey(Semester, on_delete=models.CASCADE)
+
     result_day= models.DateTimeField()
+    
 
     #class Meta:
     #    unique_together= ['mse_number','course','semester']
 
     def __str__(self):
         return f" {self.course}- {self.semester}- {self.mse_number}"
-    
+
+class mseMarks(models.Model):
+    profile= models.ForeignKey(Profile, on_delete=models.CASCADE)
+    course= models.ForeignKey(mseResult, on_delete=models.CASCADE)
+    subject= models.ForeignKey(Subject, on_delete=models.CASCADE)
+    maximum_marks= models.IntegerField()
+    obtained_marks= models.IntegerField()
+
+
+    def __str__(self):
+        return f" {self.profile}- {self.course}- {self.subject}- {self.maximum_marks}- {self.obtained_marks}"
 
